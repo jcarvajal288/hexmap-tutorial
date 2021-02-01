cca77fdfd56a2f35a134e6a3ec4ddc2897bce168
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-01 13:09:51 -0600

    blending with trapezoid edges

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -43,26 +43,32 @@ public class HexMesh : MonoBehaviour
 
     void Triangulate(HexDirection direction, HexCell cell) {
         Vector3 center = cell.transform.localPosition;
-        AddTriangle(
-            center,
-            center + HexMetrics.GetFirstCorner(direction),
-            center + HexMetrics.GetSecondCorner(direction)
-        );
+        Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+        Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+        // create solid inner triangle
+        AddTriangle(center, v1, v2 );
+        AddTriangleColor(cell.color);
+
+        // create blended outer quad
+        Vector3 v3 = center + HexMetrics.GetFirstCorner(direction);
+        Vector3 v4 = center + HexMetrics.GetSecondCorner(direction);
+        AddQuad(v1, v2, v3, v4);
         HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
         HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
         HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
-        Color edgeColor = (cell.color + neighbor.color) * 0.5f;
-        AddTriangleColor(
-            cell.color, 
-            (cell.color + prevNeighbor.color + neighbor.color) / 3f, 
+        AddQuadColor(
+            cell.color,
+            cell.color,
+            (cell.color + prevNeighbor.color + neighbor.color) / 3f,
             (cell.color + neighbor.color + nextNeighbor.color) / 3f
         );
     }
 
-    void AddTriangleColor(Color c1, Color c2, Color c3) {
-        colors.Add(c1);
-        colors.Add(c2);
-        colors.Add(c3);
+    void AddTriangleColor(Color color) {
+        colors.Add(color);
+        colors.Add(color);
+        colors.Add(color);
     }
 
     void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
@@ -73,5 +79,26 @@ public class HexMesh : MonoBehaviour
         triangles.Add(vertexIndex);
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
+    }
+
+    void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4) {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        vertices.Add(v4);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 3);
+    }
+
+    void AddQuadColor(Color c1, Color c2, Color c3, Color c4) {
+        colors.Add(c1);
+        colors.Add(c2);
+        colors.Add(c3);
+        colors.Add(c4);
     }
 }
