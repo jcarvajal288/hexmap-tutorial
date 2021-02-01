8820831bdde88fffc7cc59bd32cd867e18f24714
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-01 14:37:54 -0600

    simplify triangulation code

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -34,6 +34,6 @@ public static class HexMetrics
     }
 
     public static Vector3 GetBridge(HexDirection direction) {
-        return (corners[(int)direction] + corners[(int)direction + 1]) * 0.5f * blendFactor;
+        return (corners[(int)direction] + corners[(int)direction + 1]) * blendFactor;
     }
 }
