de99fbad3a095743eccbf36e11653def50a1afef
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-02 12:02:31 -0600

    labels move up and down with their hexes

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -9,15 +9,23 @@ public class HexCell : MonoBehaviour
     [SerializeField]
     HexCell[] neighbors;
 
+    public RectTransform uiRect;
+
     public int Elevation {
         get {
             return elevation;
         }
         set {
             elevation = value;
+            // move the cell up to match its new elevation
             Vector3 position = transform.localPosition;
             position.y = value * HexMetrics.elevationStep;
             transform.localPosition = position;
+
+            // move the UI label up as well to match
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = elevation * -HexMetrics.elevationStep;
+            uiRect.localPosition = uiPosition;
         }
     }
     int elevation;
--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -82,5 +82,6 @@ public class HexGrid : MonoBehaviour
         label.rectTransform.anchoredPosition =
             new Vector2(position.x, position.z);
         label.text = cell.coordinates.ToStringOnSeparateLines();
+        cell.uiRect = label.rectTransform;
     }
 }
