6c370ae2ae1ff15cae8fd0e69ec379d022843069
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-05 18:14:01 -0600

    Implemented camera zoom

--- Assets/Scripts/HexMapCamera.cs
+++ Assets/Scripts/HexMapCamera.cs
@@ -2,10 +2,32 @@
 
 public class HexMapCamera : MonoBehaviour
 {
+    public float stickMinZoom, stickMaxZoom;
+    public float swivelMinZoom, swivelMaxZoom;
+
     Transform swivel, stick;
+
+    float zoom = 1f;
 
     private void Awake() {
         swivel = transform.GetChild(0);
         stick = swivel.GetChild(0);
+    }
+
+    private void Update() {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta != 0f) {
+            AdjustZoom(zoomDelta);
+        }
+    }
+
+    void AdjustZoom(float delta) {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
     }
 }
