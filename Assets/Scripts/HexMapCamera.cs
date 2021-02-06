ec9dd3e3b60a48ad1d676388035ae7dcc25f1f59
Max Carvajal <jcarvajal@crimson.ua.edu>
2021-02-05 18:02:23 -0600

    Added a camera control object

---
+++ Assets/Scripts/HexMapCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class HexMapCamera : MonoBehaviour
+{
+    Transform swivel, stick;
+
+    private void Awake() {
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+}
